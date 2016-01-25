3777f987d02cc78ca253c9d2a5bc84dd55b49754
sallai <sallai.janos@vanderbilt.edu>
2016-01-25 15:40:47 -0600

    integrated mdsblocks (WiP)

---
+++ simgen/mdsblocks/editor.py
@@ -0,0 +1,68 @@
+import os
+import shutil
+
+from IPython.display import HTML, display, IFrame
+import requests
+from IPython.paths import get_ipython_dir
+
+file_path = os.path.normpath(os.path.dirname(__file__))
+ip_dir = get_ipython_dir()
+nbext_dir = os.path.join(ip_dir, 'nbextensions')
+
+mdsblocks_url = 'file://' + file_path
+mdsblocks_dir = 'mdsblocks'
+mdsblocks_files = [
+    ['mdsblocks-dist.js'],
+    ['mdsblocks-ipy.js'],
+    ['mdsblocks-dist.css'],
+    ['frame.html'],
+    ['index.html']
+    ]
+
+def install_files(url, dir, files):
+    base_url = url
+    base_path = os.path.join(nbext_dir, dir)
+    try:
+        os.makedirs(base_path)
+    except OSError:
+        pass
+    for f in files:
+        url = '/'.join([base_url]+f)
+        path = os.path.join(base_path, *f)
+        try:
+            os.makedirs(os.path.split(path)[0])
+        except OSError:
+            pass
+        if url.startswith('file://'):
+            src = url[len('file://'):]
+            shutil.copy(src, path)
+        else:
+            r = requests.get(url)
+            with open(path, 'wb') as f:
+                print('installing: %s' % path)
+                f.write(r.content)
+
+install_files(mdsblocks_url, mdsblocks_dir, mdsblocks_files)
+
+id_to_editor_map = dict()
+
+class Editor(object):
+    def __init__(self, githubToken, repoUrl):
+        self.githubToken = githubToken
+        self.repoUrl = repoUrl
+
+    def show(self):
+        # save a reference to the current editor object
+        id_to_editor_map[id(self)] = self
+        frame = """
+            <script language="text/javascript">
+                window.IPY_VARS = {
+                    OAUTH_TOKEN: "%(token)s",
+                    DEFAULT_PROJECT: "%(repo)s"
+                    EDITOR_ID: "%(id)s"
+                };
+            </script>
+        """ % {'token': self.githubToken, 'repo': self.repoUrl, 'id': id(self)}
+
+        frame += '<iframe width="99%" height="1000" src="/nbextensions/mdsblocks/index.html"></iframe>'
+        return HTML(frame)
---
+++ simgen/mdsblocks/mdsblocks-ipy.js
@@ -0,0 +1,33 @@
+function onIPyHook(editor) {
+    var browserWindow = window.parent.parent;
+
+    var token = browserWindow.IPY_VARS.OAUTH_TOKEN;
+    if(token) {
+        window.OAUTH_TOKEN = token;
+    }
+
+    var repo = browserWindow.IPY_VARS.DEFAULT_PROJECT;
+    if(repo) {
+        if(window.DEFAULT_PROJECT != repo) {
+            window.DEFAULT_PROJECT = repo;
+            editor.loadProject(repo);
+        }
+    }
+
+    var IPython = browserWindow.IPython;
+
+    // hook on 'save' action
+    document.getElementById('save').onclick = function(e) {
+        // create a command that
+        // 1. gets the editor_id to editor mapping from the module scope
+        // 2. makes editor_<editor_id> assigned to the python editor object
+        // 3. sets 'finished' property to true
+        // 4. removes the module and editor from the namespace [TODO]
+        var command = "import simgen.mdsblocks.editor as smds_ed;" +
+            "editor_" + IPY_VARS.EDITOR_ID + " = smds_ed.id_to_editor_map[" + IPY_VARS.EDITOR_ID + "];" +
+            "editor_" + IPY_VARS.EDITOR_ID + ".finished = True";
+        console.log("Executing command " + command);
+        var kernel = IPython.notebook.kernel;
+        kernel.execute(command);
+    }
+}
